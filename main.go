b40151364592ae8f884d663448d253bd0f581d3d
Mohammed Damia <contacts@primetuts.com>
2018-09-03 19:49:13 -0700

    more maps example

--- main.go
+++ main.go
@@ -5,6 +5,27 @@ import (
 )
 
 func main() {
-	log.Println("Bare Bone go project")
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// 	"gray":  "#898989",
+	// }
+	// log.Println(colors)
 
+	// person := make(map[string]string)
+	// person["name"] = "Mohammed"
+	// person["age"] = "40"
+	// log.Println(person)
+
+	status := make(map[int]string)
+	status[1] = "good"
+	status[0] = "bad"
+	printMap(status)
+
+}
+
+func printMap(c map[int]string) {
+	for number, desc := range c {
+		log.Println(number, desc)
+	}
 }
